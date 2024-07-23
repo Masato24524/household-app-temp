39b5b6a25e11740619ff4c244ed89681d7b93f49
masato <masato19729@gmail.com>
2024-07-23 17:30:22 +0900

    🚀 家計簿アプリの一時リリース

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ChartOptions
+  } from "chart.js";
+  import { Bar } from "react-chartjs-2";
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+const BarChart = () => {
+    const options: ChartOptions<"bar">
+      = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top"
+          },
+          title: {
+            display: true,
+            text: "Chart.js Bar Chart"
+          }
+        }
+      };
+
+    const labels = ["2024-07-18", "February", "March", "April", "May", "June", "July"];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+            label: "支出",
+            data: [100, 200, 300, 400, 500, 600, 700],
+            backgroundColor: "rgba(255, 99, 132, 0.5)"
+            },
+            {
+            label: "収入",
+            data: [100, 200, 300, 400, 500, 600, 700],
+            backgroundColor: "rgba(53, 162, 235, 0.5)"
+            }
+        ]
+    };
+  return (
+    <Bar options={options} data={data} />
+  )
+}
+
+export default BarChart
---
+++ src/components/MonthSelector.tsx
@@ -0,0 +1,25 @@
+import { Box, Button } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import React from 'react'
+
+const MonthSelector = () => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <Button color={"error"} variant="contained">
+          先月
+        </Button>
+        <div>
+          <DatePicker sx={{ mx: 2 }}/>
+        </div>
+        <Button color={"primary"} variant="contained">
+          次月
+        </Button>
+      </Box>
+    </LocalizationProvider>
+  )
+}
+
+export default MonthSelector
