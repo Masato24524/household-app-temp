69d10336cd3ed8c907b96bbf3c72b185d671197c
Masato24524 <masato24524@gmail.com>
2025-03-21 15:08:09 +0900

    🐛 ESmodules形式に修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    // output: "export",
-    // basePath: process.env.NODE_ENV === 'production' ? "/household-app-temp" : "",
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: '/',
-    //             destination: '/', // リダイレクト先のパス
-    //             permanent: true,
-    //         }
-    //     ]
-    // }
-    trailingSlash: true,
-    output: 'standalone',
-}
-
-=======
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -36,5 +16,4 @@ const nextConfig = {
     output: 'standalone',
 }
 
->>>>>>> efca81d441df5ac008df521069edf8fd64f395d6
 export default nextConfig;
